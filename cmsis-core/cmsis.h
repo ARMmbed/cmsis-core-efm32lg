a096d958510ed658f3a5d01302213f7fcc12fca5
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2015-11-30 10:38:02 +0100

    Add device header files for EFM32LG.

---
+++ cmsis-core/cmsis.h
@@ -0,0 +1,17 @@
+/* mbed Microcontroller Library - CMSIS
+ * Copyright (C) 2009-2011 ARM Limited. All rights reserved.
+ *
+ * A generic CMSIS include header, pulling in EFM32 specifics
+ */
+
+#ifndef MBED_CMSIS_H
+#define MBED_CMSIS_H
+
+#include <stdint.h>
+extern uint32_t __start_vector_table__;
+
+#include "cmsis-core/em_device.h"
+#include "cmsis-core/cmsis_nvic.h"
+#include "em_mbed.h"
+
+#endif
